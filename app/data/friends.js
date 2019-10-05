c8b1c10c62d656ddf2e69eb056bec03c6f92c823
Guillermo Villalta <mexcelus@gmail.com>
2019-10-04 22:02:36 -0600

    added freddy k

--- app/data/friends.js
+++ app/data/friends.js
@@ -21,8 +21,23 @@ var tableArray = [
             4,
             1
         ]
+    },
+    {
+        "name": "Freddy Krueger",
+        "photo": "https://www.dreadcentral.com/wp-content/uploads/2019/05/Freddy-Krueger-Banner.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
     }
-
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
